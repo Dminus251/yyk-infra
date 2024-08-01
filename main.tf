403d529837445d54eb88869e25bf4f7ba5590528
Dminus251 <yyk0370@naver.com>
2024-08-01 17:07:59 +0900

    first commit

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ module "vpc" {
   source 	= "./modules/terraform-aws-vpc"  # 로컬 모듈 경로
   vpc_name      = "my-vpc"
   vpc_cidr	= "10.0.0.0/16"
-  #availability_zones = ["ap-northeast-2a", "ap-northeast-2b"]
 }
 
 
@@ -10,7 +9,6 @@ module "subnet" {
   source        = "./modules/terraform-aws-subnet"
   count         = length(var.availability_zones) * 2  # 두 개의 가용 영역에 대해 각각 2개의 서브넷
   cidr_block    = cidrsubnet(module.vpc.vpc_cidr, 4, count.index)  # 서브넷의 CIDR 블록 계산
-  #availability_zone = element(var.availability_zones, floor(count.index / 2))  # 가용 영역 할당
   availability_zone = var.availability_zones[floor(count.index/2)]  # 가용 영역 할당
   vpc_id        = module.vpc.vpc_id
 }
--- modules/terraform-aws-subnet/main.tf
+++ modules/terraform-aws-subnet/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "main" {
-  count = 1  # 실제로는 루트 모듈에서 설정된 count를 따릅니다.
+  count = 1 #이거 안 하면 tags에서 에러남..
 
   vpc_id                  = var.vpc_id
   cidr_block              = var.cidr_block
